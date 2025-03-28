6304dd19f07367565927adef9c83146b4bc098fa
Pumplies <maxmax20051120@gmail.com>
2025-03-28 11:33:47 +0500

    met

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,12 +19,24 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="viewport" content={metadata.viewport} />
+        <meta
+          property="og:image"
+          content="https://artsoffio-b2b.com/logoBurger.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:title" content="Косметика ART SOFFIO оптом" />
+        <meta
+          property="og:description"
+          content="Надежная косметика для бизнеса"
+        />
+        <meta property="og:url" content="https://artsoffio-b2b.com" />
+
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`antialiased ${montserrat.className}`}>
-        <ClientWrapper>
-          {children}
-        </ClientWrapper>
+        <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
   );
