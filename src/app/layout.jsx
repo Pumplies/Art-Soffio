698185a91b4ddd8a1823c7bba3db87ad665c29a0
Pumplies <maxmax20051120@gmail.com>
2025-04-02 14:41:24 +0500

    google

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -19,21 +20,32 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="viewport" content={metadata.viewport} />
-        <meta
-          property="og:image"
-          content="https://artsoffio-b2b.com/logoBurger.png"
-        />
+        <meta property="og:image" content="https://artsoffio-b2b.com/logoBurger.png" />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:title" content="Косметика ART SOFFIO оптом" />
-        <meta
-          property="og:description"
-          content="Надежная косметика для бизнеса"
-        />
+        <meta property="og:description" content="Надежная косметика для бизнеса" />
         <meta property="og:url" content="https://artsoffio-b2b.com" />
-
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16969659650"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16969659650');
+            `,
+          }}
+        />
       </head>
       <body className={`antialiased ${montserrat.className}`}>
         <ClientWrapper>{children}</ClientWrapper>
